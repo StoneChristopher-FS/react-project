0fd588bca18cc9a66485fed757ac67806df012e4
Christopher Stone <cmstone@student.fullsail.edu>
2021-12-13 12:01:52 -0500

    Add page header component

---
+++ src/components/PageHeader.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const PageHeader = props => {
+    return (
+        <header style={styles.header}>
+            <div>
+                <h1 style={styles.title}>{props.title}</h1>
+            </div>
+            <div style={styles.right}>
+                <img src={props.profileImg} alt="Profile avatar" style={styles.img} />
+                <p>{props.name}</p>
+            </div>
+
+        </header>
+    )
+}
+
+export default PageHeader
+
+const styles = {
+    header: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '15px 0',
+        backgroundColor: '#003C52',
+        color: '#F3E5CE'
+    },
+    title: {
+        margin: '0',
+        paddingLeft: '10px'
+    },
+    right: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        width: '10%',
+        paddingRight: '10px'
+    },
+    img: {
+        height: '45px',
+        borderRadius: '25%'
+    }
+}
